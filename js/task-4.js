083b11d4d6ab55b977f8afd68c451966c34e3eea
Denys Stepanenko <denys.workspace@gmail.com>
2025-03-10 02:43:46 -0600

    Fix getShippingCost function to use strict equality and improve output messages

--- js/task-4.js
+++ js/task-4.js
@@ -1,25 +1,32 @@
 'use strict';
 function getShippingCost(country) {
     let price;
-    if (country = "China") {
+    if (country === "China") {
         price = 170;
-    } else if (country = "Chile") {
+    } else if (country === "Chile") {
         price = 250;
-    } else if (country = "Australia") {
+    } else if (country === "Australia") {
         price = 170;
-    } else if (country = "Jamaica") {
+    } else if (country === "Jamaica") {
         price = 120;
     } else {
         return "Sorry, there is no delivery to your country"
     }
     switch (country) {
         case "China":
-            return "Shipping to China will cost 100 credits";
+            return `Shipping to ${country} will cost ${price} credits`;
         case "Chile":
-            return "Shipping to Chile will cost 250 credits";
+            return `Shipping to ${country} will cost ${price} credits`;
         case "Australia":
-            return "Shipping to Australia will cost 170 credits";
+            return `Shipping to ${country} will cost ${price} credits`;
         case "Jamaica":
-            return "Shipping to Jamaica will cost 120 credits";
+            return `Shipping to ${country} will cost ${price} credits`;
     }
 }
+
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden"));
