64d6d82c84c057208c0cef5d4e5dbfb218038ed1
Denys Stepanenko <denys.workspace@gmail.com>
2025-03-10 00:04:52 -0600

    Add validation for customer credits in makeTransaction function

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,10 @@
 'use strict';
 function makeTransaction(quantity, pricePerDroid, custumerCredits) {
     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    if (custumerCredits >= totalPrice) {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    } else {
+        return "Insufficient funds!"
+    }
 }
-console.log(makeTransaction(5, 3000,));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000, 23000)); 
